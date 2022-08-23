c489878cfb75438f37eb04c655a2fcb49e743052
Giuliana Girardelli <105557992+girardelligiuliana@users.noreply.github.com>
2022-08-23 14:26:16 +0100

    Update Unemployment Rate of Persons 25-64 years.py

--- Unemployment Rate of Persons 25-64 years.py
+++ Unemployment Rate of Persons 25-64 years.py
@@ -1,210 +1,44 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd #read CSV files, data processing
 import numpy as np #recomendation system
 import matplotlib.pyplot as plt #data visualisation
 import seaborn as sns #data visualisation
 
-
-# In[2]:
-
-
 df = pd.read_csv(r"C:\Users\girar\OneDrive\Escritorio\Unemployment Rate of Persons 25-64 years.csv")
 #import csv file
 
-
-# In[3]:
-
-
 df.info()
-
-
-# In[4]:
-
 
 df.shape
 
-
-# In[5]:
-
-
 df.head()
-
-
-# In[6]:
-
 
 df.tail()
 
-
-# In[7]:
-
-
 df.describe() 
-
-
-# In[ ]:
-
-
-
-
-
-# In[8]:
-
 
 df.columns
 
+df.duplicated().sum() 
 
-# In[9]:
+df.isnull() 
 
+df.isnull().sum()
 
-df.duplicated().sum() # there are not duplicates in the database
-
-
-# In[10]:
-
-
-df.isnull() # returns a specified value if the expression is NULL
-
-
-# In[11]:
-
-
-df.isnull().sum() #missing values in the database
-
-
-# In[12]:
-
-
-df.dropna(axis=0, how="any", inplace=True) #drop all rows with missing values and overwrite dataset
-
-
-# In[ ]:
-
-
-
-
-
-# In[13]:
-
+df.dropna(axis=0, how="any", inplace=True)
 
 df.sort_values("Education Level")
 
-
-# In[14]:
-
-
 df[["Sex", "Education Level", "VALUE"]]
-
-
-# In[15]:
-
 
 df[["VALUE", "Education Level", "Quarter"]]
 
-
-# In[16]:
-
-
 df[df["Sex"] == "Female"]
-
-
-# In[ ]:
-
-
-
-
-
-# In[17]:
-
 
 df[df["Sex"] == "Male"]
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[18]:
-
-
 df.groupby("Sex")["VALUE"].mean()
 
-
-# In[19]:
-
-
 df.groupby("Sex")["VALUE"].agg([min, max, sum])
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[45]:
-
 
 #Unemployment rates Female vs Male
 df[df["Sex"] == "Female"]["VALUE"].hist(alpha=0.7)
@@ -214,50 +48,20 @@ plt.legend(["Female", "Male"])
 plt.title("Unemployment Rate Female VS Male")
 plt.show()
 
+df[df["Sex"] == "Female"].hist(alpha=0.7)
 
-# In[ ]:
-
-
-
-
-
-# In[21]:
-
-
-av_rate_by_sex = df.groupby("Sex")["VALUE"].mean()
-print(av_rate_by_sex)
-
-av_rate_by_sex.plot(kind="bar", title="Mean Unemployment Rate by Sex")
+plt.title("Female Unemployment Rate ")
 plt.show()
 
-#unemploment rate female lower than men
 
-
-# In[ ]:
-
-
-
-
-
-# In[23]:
-
-
-# grafico de torta con education level solo de female, y solo de male
-
-# grafico lineal primario a traves del tiempo, secundario etc
-
-
-# In[36]:
-
+df[df["Sex"] == "Male"].hist()
+plt.title("Male Unemployment Rate ")
+plt.show()
 
 df.plot(x="Quarter", y="VALUE", kind="line", title="Unemployment Rate through Time")
 plt.show() 
 
 #unemployment highest rate 2012 q2, lowest 2020q2
-
-
-# In[27]:
-
 
 
 av_rate_by_Education_Level = df.groupby("Education Level")["VALUE"].mean()
@@ -267,10 +71,3 @@ av_rate_by_Education_Level.plot(kind="bar", title="Unemployment Rate Mean by Edu
 plt.show()
 
 #unemployment highest rate primary
-
-
-# In[ ]:
-
-
-
-
